f79852019e5e7ac6aa491e5f0c77153e512b7925
xserafineq <serafin24m@gmail.com>
2026-05-23 17:10:29 +0200

    nawigacja strony, inicjalizacja layoutu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+
+</head>
+<body>
+@extends('layout')
+
+@section('content')
+    <h1>To jest treść strony głównej</h1>
+@endsection
+</body>
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+      <title>Mamto.pl</title>
+     @vite(['resources/css/app.css'])
+  </head>
+  <body>
+    <nav>
+        <div id="logo">
+            <img src="{{ asset('assets/logo.png') }}" alt="logo"/>
+        </div>
+        <div id="search-box">
+            <input id="search" type="text" placeholder="szukaj">
+            <button id="filter-btn"><img src="{{ asset('assets/fi-ss-filter.png') }}"></button>
+        </div>
+        <ul id="menu">
+            <li><img src="{{ asset('assets/fi-bs-heart.png') }}" alt="favourites"/></li>
+            <li><img src="{{ asset('assets/messages.png') }}" alt="messages"/></li>
+            <li><img src="{{ asset('assets/fi-sr-user.png') }}" alt="user-profile"/></li>
+        </ul>
+    </nav>
+    <main>
+        @yield('content')
+    </main>
+  <footer>
+
+  </footer>
+  </body>
+</html>
