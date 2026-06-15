8fc2326481a99a26498e0fd04a829fb2b7861ad3
Sulmo <sul_morski@wp.pl>
2026-06-15 17:20:27 +0200

    poprawa z-index nawigacji

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
   </head>
   <body>
-    <nav>
+    <nav id="site-nav">
         <div id="logo">
             <a href="/">
                 <img src="{{ asset('assets/logo.png') }}" alt="logo"/>
